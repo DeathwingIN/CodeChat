b744665bbc54fb6039367998c5c99021b5319634
DeathwingIN <imeshnirmal1u@gmail.com>
2023-04-05 08:49:50 +0530

    change server address

--- client/main.js
+++ client/main.js
@@ -89,7 +89,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv)
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codechat-kr84.onrender.com', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
